c9831300f121f093b9304e120e6782add2c908fc
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-18 17:02:12 +0530

    Add example usage of Deque in deque.java
    Implemented a basic example of using Deque in Java.

---
+++ deque.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+// 		Stack<Integer> stack = new Stack<>();
+// 		stack.push(45);
+// 		stack.push(23);
+// 		stack.push(65);
+// 		stack.push(56);
+// 		stack.push(123);
+		
+// 		System.out.println(stack.pop());
+// 		System.out.println(stack.pop());
+// 		System.out.println(stack.pop());
+// 		System.out.println(stack.pop());
+// 		System.out.println(stack.pop());
+// 				System.out.println(stack.pop());
+
+// Queue<Integer> queue = new LinkedList<>();
+// queue.add(3);
+// queue.add(6);
+// queue.add(5);
+// queue.add(4);
+// System.out.println(queue.peek());
+// System.out.println(queue.remove());
+      
+      
+      Deque<Integer> deque = new ArrayDeque<>();
+      deque.add(64);
+      deque.addLast(34);
+      deque.removeFirst();
+		
+
+	}
+}
