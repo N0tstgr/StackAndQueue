d2ea7bae9ab0904bb2fbdf02b34cf0e6b87b1e87
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-18 16:09:18 +0530

    Add introstack.java to demonstrate stack operations

---
+++ introstack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Stack<Integer> stack = new Stack<>();
+		stack.push(45);
+		stack.push(23);
+		stack.push(65);
+		stack.push(56);
+		stack.push(123);
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+
+	}
+}
