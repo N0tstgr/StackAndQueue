a4fb78a3f9eff2656ff0a3b397747faf5064daf1
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-22 18:01:31 +0530

    Implement CustomStack class with default and parameterized constructor

---
+++ CustomStackagain.java
@@ -0,0 +1,19 @@
+package ArrayDeque;
+
+public class CustomStack {
+    protected int[] data;
+private static  final int  DEFAULT_SIZE = 11;
+
+
+public CustomStack(){
+    this.data = new int[DEFAULT_SIZE];
+}
+public CustomStack(int size){
+    this.data = new int[size];
+
+}
+
+
+
+}
+
